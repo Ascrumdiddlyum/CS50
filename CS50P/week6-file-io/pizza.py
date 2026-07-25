4d93fcec4984b5f3c2fda6e7e033d61d4b56b79f
Ghali Kadira <kadiraghali@gmail.com>
2026-07-25 15:56:49 +0000

    Fix filepath in code

--- CS50P/week6-file-io/pizza.py
+++ CS50P/week6-file-io/pizza.py
@@ -3,19 +3,22 @@ import csv
 from tabulate import tabulate
 
 #Verifies if there is exactly one command-line argument
-if len(sys.argv) > 2:
-    sys.exit("Too many command-line arguments")
-if len(sys.argv)< 2:
-    sys.exit("Too few command-line arguments")
+def verify(argument):
+    if len(argument) > 2:
+        sys.exit("Too many command-line arguments")
+    if len(argument)< 2:
+        sys.exit("Too few command-line arguments")
 
 #Tabulates the inputted CSV file
-try:
-    with open(sys.argv[1]) as file:
-        reader = csv.DictReader(file)
-        for row in reader:
+def print_table(argument):
+    try:
+        with open(f"data/pizza/{argument[1]}") as file:
+            reader = csv.reader(file)
             headers = next(reader)
             table = list(reader)
-except FileNotFoundError:
-    sys.exit("File doesn't exist")
+    except FileNotFoundError:
+        sys.exit("File doesn't exist")
+    print(tabulate(table, headers, tablefmt="grid"))
 
-print(tabulate(table, headers, tablefmt="grid"))
+verify(sys.argv)
+tabulate(sys.argv)
--- CS50P/week6-file-io/scourgify.py
+++ CS50P/week6-file-io/scourgify.py
@@ -1,26 +1,33 @@
 import csv
 import sys
 
-if len(sys.argv) < 3:
-    sys.exit("Too few command-line arguments")
-if len(sys.argv) > 3:
-    sys.exit("Too many command-line arguments")
+def verify(argument):
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    for arg in argument[-2:]:
+        if not arg.endswith(".csv"):
+            sys.exit("Not a CSV file")
 
-try:
-    with open("before.csv") as file:
-        reader = csv.reader(file)
-        next(reader)
-        with open(sys.argv[2], "w") as newfile:
-            fieldnames = ["first", "last", "house"]
-            writer = csv.DictWriter(newfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for row in reader:
-                name, house = row
-                last, first = name.split(", ")
-                writer.writerow({"first": first, "last": last, "house": house})
+def rewrite_names(argument):
+    try:
+        with open(f"data/scourgify/{argument[1]}") as file:
+            reader = csv.reader(file)
+            next(reader)
+            with open(f"data/scourgify/{argument[2]}", "w") as newfile:
+                fieldnames = ["first", "last", "house"]
+                writer = csv.DictWriter(newfile, fieldnames=fieldnames)
+                writer.writeheader()
+                for row in reader:
+                    name, house = row
+                    last, first = name.split(", ")
+                    writer.writerow({"first": first, "last": last, "house": house})
+    except FileNotFoundError:
+        sys.exit(f"Could not read {argument[1]}")
 
-except FileNotFoundError:
-    sys.exit(f"Could not read{sys.argv[1]}")
+verify(sys.argv)
+rewrite_names(sys.argv)
 
             
 
